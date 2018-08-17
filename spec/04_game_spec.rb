48c9d63f79ac5f9e78feeaecba41e83585aae513
Randall Robinson <keith_adams007@yahoo.com>
2018-08-17 00:40:45 +0000

    automatically backed up by learn

--- spec/04_game_spec.rb
+++ spec/04_game_spec.rb
@@ -2,52 +2,52 @@ require 'spec_helper'
 
 describe 'Game' do
   describe '::WIN_COMBINATIONS' do
-  
+
   end
 
   describe '#board' do
-  
+
   end
 
   describe '#player_1' do
-    
+
   end
 
   describe '#player_2' do
-    
+
   end
 
   describe 'initialize' do
-    
+
   end
 
   describe '#current_player' do
-    
+
   end
 
   describe '#over?' do
-  
+
   end
 
   describe '#won?' do
-    
+
   end
 
   describe '#draw?' do
-    
+
   end
 
   describe '#winner' do
-  
+
   end
 
   describe 'turn' do
-    
+
   end
 
   describe 'play' do
-  
-  
+
+
 
   end
 
