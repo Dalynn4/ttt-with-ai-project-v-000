538aee4930df3114c7e94592f4202bb5bce83f36
Randall Robinson <keith_adams007@yahoo.com>
2018-08-17 00:38:44 +0000

    automatically backed up by learn

--- spec/01_board_spec.rb
+++ spec/01_board_spec.rb
@@ -4,35 +4,35 @@ describe "Board" do
   let(:board){Board.new}
 
   describe '#cells' do
-  
+
   end
 
   describe '#reset!' do
-  
+
   end
 
   describe '#initialize' do
-  
+
   end
 
   describe '#display' do
-  
+
   end
 
   describe '#position' do
-    
+
   end
 
   describe '#full?' do
-    
+
   end
 
   describe '#turn_count' do
-    
+
   end
 
   describe '#taken?' do
-  
+
   end
 
   describe '#valid_move?' do
@@ -40,6 +40,6 @@ describe "Board" do
   end
 
   describe '#update' do
-    
+
   end
 end
